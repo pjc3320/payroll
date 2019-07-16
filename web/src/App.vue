fd5d731d44e327f74bc7e39536b3c5bf9f482843
Paul Crossan <paul.crossan@dudesolutions.com>
2019-07-16 14:25:17 -0400

    basic page framework; wip

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Payroll />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Payroll from './components/Payroll.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Payroll
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
