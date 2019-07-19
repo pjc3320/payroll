e69bf9fb4144726026251f8f7f565599fc5eb636
paul crossan <paul.crossan@gmail.com>
2019-07-18 20:01:38 -0400

    tweak UI

--- web/src/components/Payroll.vue
+++ web/src/components/Payroll.vue
@@ -2,12 +2,20 @@
   <div class="page">
     <v-flex s12>
       <v-flex s10>
-        <v-card elevation24 align-center>
-          <v-card-title>Employees</v-card-title>
-          <v-data-table :headers="headers" :items="employees" class="elevation-1">
+        <v-card elevation24 align-center color="primary">
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline">Employees</h3>
+            </div>
+            <v-btn fab small color="cyan accent-2" bottom left absolute @click="dialog = !dialog">
+              <v-icon>add</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-data-table :headers="headers" :items="employees" class="elevation-3">
             <template v-slot:items="props">
               <td>{{ props.item.firstName }}</td>
               <td>{{ props.item.lastName }}</td>
+              <td class="text-xs-center">{{ props.item.dependentCount }}</td>
               <td class="text-xs-right">{{ props.item.benefitCost }}</td>
             </template>
           </v-data-table>
@@ -18,6 +26,9 @@
 </template>
 
 <style lang="scss">
+h3.headline {
+  color: "white";
+}
 </style>
 <script>
 export default {
@@ -41,6 +52,13 @@ export default {
           dataType: "String"
         },
         {
+          text: "# Dependents",
+          align: "center",
+          sortable: true,
+          value: "dependentCount",
+          dataType: "Numeric"
+        },
+        {
           text: "Benefit Cost",
           align: "right",
           sortable: true,
@@ -52,16 +70,19 @@ export default {
         {
           firstName: "Eli",
           lastName: "Manning",
+          dependentCount: 3,
           benefitCost: 1000
         },
         {
           firstName: "Saquon",
           lastName: "Barkley",
+          dependentCount: 2,
           benefitCost: 1000
         },
         {
           firstName: "Pete",
           lastName: "Alonso",
+          dependentCount: 0,
           benefitCost: 1000
         }
       ]
