6012eff087569c64074ceda22a5d1fccb3c7a102
paul crossan <paul.crossan@gmail.com>
2019-07-19 21:13:02 -0400

    add repository

---
+++ api/Payroll.Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Xsl;
+
+namespace Payroll.Data
+{
+    public interface IRepository<T>
+    {
+        Task<T> Get(Guid id);
+
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> Upsert(T entity);
+    }
+}
---
+++ api/Payroll.Data/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Couchbase;
+using Couchbase.Core;
+
+namespace Payroll.Data
+{
+    public abstract class Repository<T> : IRepository<T>
+    {
+        private readonly IBucket _bucket;
+
+        protected Repository(IBucket bucket)
+        {
+            _bucket = bucket;
+        }
+
+        public async Task<T> Get(Guid id)
+        {
+            var result = await _bucket.GetDocumentAsync<T>(id.ToString());
+
+            return result.Content;
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Upsert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
